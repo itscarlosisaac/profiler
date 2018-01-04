a0ba9db2b43baefb61de4b3a68352ca3609f9be9
Carlos Isaac Martinez Batista <itscarlosisaac@gmail.com>
2018-01-04 18:27:11 -0400

    Add: Main app.js file

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import HomePage from './components/Home'
+
+
+// Import Styles
+
+const root = document.getElementById('app');
+ReactDOM.render(<HomePage />, root );
