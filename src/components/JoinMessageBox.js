283b3d9b6a1830a2e5a8add3e91ae0fdc7c40915
Carlos Isaac Martinez Batista <itscarlosisaac@gmail.com>
2018-01-04 18:57:53 -0400

    Add: JoinMessageBox Component.

---
+++ src/components/JoinMessageBox.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class JoinMessageBox extends Component {
+    render () {
+        return (
+            <div>
+                Join the toptal community. Apply to work with us as a 
+                <button>Developer</button>
+                or
+                <button>Client</button>
+            </div>
+        )
+    }
+}
+
+export default JoinMessageBox
