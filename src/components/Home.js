044ade165b962afb8dbb2e8c58353fb269400255
Carlos Isaac Martinez Batista <itscarlosisaac@gmail.com>
2018-01-04 18:27:53 -0400

    Add: Home main component.

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import Header from './common/Header'
+import Footer from './common/Footer'
+
+
+class HomePage extends Component {
+    render () {
+        return (
+            <div>
+                <Header />
+                HomePage
+                <Footer />
+            </div>
+        )
+    }
+}
+
+export default HomePage
