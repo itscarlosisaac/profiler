0fa8c870f2b74b02d51319639ee286b12f7fdce4
Carlos Isaac Martinez Batista <itscarlosisaac@gmail.com>
2018-01-04 18:27:44 -0400

    Add: Common components for page.

---
+++ src/components/common/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Navigation from './Navigation'
+
+class Header extends Component {
+    render () {
+        return (
+            <div>
+                Header
+                <Navigation />
+            </div>
+        )
+    }
+}
+
+export default Header
