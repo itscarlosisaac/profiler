a33c63e428e0d103ff2e5d36e22400a710f4cfb4
Carlos Isaac Martinez Batista <itscarlosisaac@gmail.com>
2018-01-04 18:55:12 -0400

    Add: TopSkills component.

---
+++ src/components/ToptalTopSkills.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class ToptalTopSkills extends Component {
+    render () {
+        return (
+            <div>
+                <h3>Top skills on toptal</h3>
+                <div>
+                    Listing here
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ToptalTopSkills
