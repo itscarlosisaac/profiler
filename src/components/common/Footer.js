87ec070510fa9ec14a249ef17c9a59af8cf833b8
Carlos Isaac Martinez Batista <itscarlosisaac@gmail.com>
2018-01-04 18:54:56 -0400

    Edit: Content for the footer.

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -4,7 +4,44 @@ class Footer extends Component {
     render () {
         return (
             <div>
-                Footer
+                <div>
+                    <h4>About toptal</h4>
+                    <p>
+                        TopTal connects start-ups businesses. and organizations to a growing network of the best developers in the world. Our engineers are available full- or part-time and are able to seamlessly integrate into your team.
+                    </p>
+                </div>
+
+                <div>
+                    <h4>FAQ</h4>
+                    <ul>
+                        <li>How can i join toptal?</li>
+                        <li>What requirements i have to meet?</li>
+                        <li>How do you pay me?</li>
+                        <li>Lorem ipsuum dolor it amet?</li>
+                        <li>Rokok ini nulla facilis?</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <ul>
+                        <li><a href="#">What</a></li>
+                        <li><a href="#">Why</a></li>
+                        <li><a href="#">How</a></li>
+                        <li><a href="#">Clients</a></li>
+                        <li><a href="#">Team</a></li>
+                        <li><a href="#">Lab</a></li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h4>Contact</h4>
+                    <ul>
+                    <li><a href="#">Join us</a></li>
+                    <li><a href="mailto:email@gmail.com">Send us e-mail</a></li>
+                    <li><a href="#">Follow us on twitter</a></li>
+                    <li><a href="#">Call 888.323.4422</a></li>
+                    </ul>
+                </div>
             </div>
         )
     }
