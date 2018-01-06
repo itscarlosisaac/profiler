884d14b7519bd66f8b1ba52fcb1a0143ce200f73
Carlos Isaac Martinez Batista <itscarlosisaac@gmail.com>
2018-01-06 12:41:53 -0400

    Edit: Join message box content and styles.

--- src/components/JoinMessageBox.js
+++ src/components/JoinMessageBox.js
@@ -3,11 +3,18 @@ import React, { Component } from 'react'
 class JoinMessageBox extends Component {
     render () {
         return (
-            <div>
-                Join the toptal community. Apply to work with us as a 
-                <button>Developer</button>
-                or
-                <button>Client</button>
+            <div className="app__join__message__box">
+                <div className="row">
+                    <div className="columns  align-self-middle large-12">
+                        <p className="text__block">
+                            Join the toptal community. 
+                            <span className="text__block--white"> Apply to work with us as a </span> 
+                            <button className="app__btn app__btn--join--message--box">Developer</button>
+                            <span className="text__block--italic">  or </span>
+                            <button className="app__btn app__btn--join--message--box">Client</button>
+                        </p>
+                    </div>
+                </div>
             </div>
         )
     }
