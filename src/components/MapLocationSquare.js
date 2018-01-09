ab2770e5020294e7e8dbd10677948547eeb4d8bc
Carlos Isaac Martinez Batista <itscarlosisaac@gmail.com>
2018-01-09 13:02:49 -0400

    Edit: Map location sync with data.

--- src/components/MapLocationSquare.js
+++ src/components/MapLocationSquare.js
@@ -1,47 +1,75 @@
 import React, { Component } from 'react'
 import googleMapClient from '@google/maps'
 import GoogleMapsLoader from 'google-maps'
-import { setTimeout } from 'timers';
 
 class MapLocationSquare extends Component {
     constructor(props){
         super(props);
         this.state = {
-            address: 'Portland, Oregon, USA',
-            geolocation: {lat: -25.363, lng: 131.044}
+            address: this.props.address,
+            geolocation: {}
         }
     }
+
     getLocation(){
         const Client = googleMapClient.createClient({
             key: 'AIzaSyCiljKtouaMaT1cr5cX3I9D1DzsbHC3iBc',
             Promise: Promise 
         });
 
-        Client.geocode({address: this.state.address }).asPromise()
+        // this.setState({ address:this.props.address });
+
+        Client.geocode({ address: this.state.address }).asPromise()
             .then((response) => {
-                console.log(response.json.results)
                 this.setState((prevState, nextState ) => ({
                     geolocation: response.json.results[0].geometry.location 
                 }))
+            })
+            .then(() => {
+                this.createMap();
             })
             .catch((err) => {
                 console.log(err);
             });
     }
+
     createMap(){
         GoogleMapsLoader.KEY = 'AIzaSyCiljKtouaMaT1cr5cX3I9D1DzsbHC3iBc';
         const el = document.getElementById('map');
+        
         const options = {
-            center: this.state.getLocation,
-            zoom: 8
+            center: this.state.geolocation,
+            zoom: 12
         }
+
         GoogleMapsLoader.load( function(google) {
             const map =  new google.maps.Map(el, options);
+            const marker = new google.maps.Marker({
+                position: options.center,
+                map: map
+            })
         });
     }
-    componentDidMount(){
-        this.createMap();
+
+    componentWillReceiveProps(nextProps){
+        if( nextProps.address !== this.props.address ){
+            new Promise( ( resolve, reject ) => {
+                this.setState({ address:nextProps.address })
+                resolve();
+            })
+            .then( () => {
+                this.getLocation()
+            })
+            .catch((e) => {
+                console.log(e);
+            })
+        }
     }
+
+    componentDidMount(){
+        this.getLocation();
+    }
+
     render () {
         return (
             <div>
