1deda6c04e397c1f509ea981b15c907a1f358bdc
Carlos Isaac Martinez Batista <itscarlosisaac@gmail.com>
2018-01-10 19:17:25 -0400

    Edit: Data model.

---
+++ src/components/utils/Model.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+    profileData:{
+        name: '',
+        location: '',
+        languages: '',
+        skills: [],
+        resumeFile: ''
+    },
+    portfolio: [],
+    experience: [],
+    sampleCode: '',
+    availability: '',
+    preferedEnviroments: [],
+    map: '',
+    quotes: [],
+    projects: [ ]
+}
