a2fb5f7ea898c732141ca8f46d8f2662e8b40591
Carlos Isaac Martinez Batista <itscarlosisaac@gmail.com>
2018-01-10 23:10:54 -0400

    Edit: Portfolio box.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,163 @@
-import React, { Component } from 'react'
-import Header from './common/Header'
-import ToptalTopSkills from './ToptalTopSkills'
-import JoinMessageBox from './JoinMessageBox'
-import Footer from './common/Footer'
+import React, { Component } from "react";
+import Header from "./common/Header";
+import ProfileInfo from "./ProfileInfo";
+import PortfolioSuqare from "./PortfolioSquare";
+import ExperienceSquare from "./ExperienceSquare";
+import SampleCodeSquare from "./SampleCodeSquare";
+import AvailabilitySquare from "./AvailabilitySquare";
+import QuoteSquare from "./QuoteSquare";
+import MapLocationSquare from "./MapLocationSquare";
+import ToptalTopSkills from "./ToptalTopSkills";
+import JoinMessageBox from "./JoinMessageBox";
+import Footer from "./common/Footer";
+import PortfolioSquare from "./PortfolioSquare";
+import { setTimeout } from "timers";
+import MODEL from "./utils/Model";
 
 
 class HomePage extends Component {
-    render () {
-        return (
-            <div>
-                <Header />
-                HomePage
-                <ToptalTopSkills />
-                <JoinMessageBox/>
-                <Footer />
-            </div>
-        )
-    }
+  constructor(props) {
+      super(props);
+      this.state = {
+          editMode: false,
+          profileData: {
+              imageURL: "http://www.placehold.it/240x240",
+              name: "Add Name",
+              address: "Add Location",
+              languages: "Add Languages",
+              skills: [],
+              resumeFile: ''
+          },
+          portfolio: [],
+          experience: [],
+          sampleCode: '',
+          preferedEnviroments: [],
+          availability: '',
+          projects:[],
+          quotes: [],
+      };
+  }
+
+  componentWillMount() {
+      const Data =
+          JSON.parse(localStorage.getItem("toptal-profile")) ||
+          localStorage.setItem("toptal-profile", JSON.stringify(MODEL));
+
+      if (Data !== {} && Data !== undefined) {
+          this.setState((prevState, nextState) => ({
+              profileData: {
+                  imageURL: Data.imageURL,
+                  name: Data.name,
+                  languages: Data.languages,
+                  address: Data.address,
+                  skills: Data.skills,
+                  resumeFile: Data.resumeFile
+              },
+              portfolio: Data.portfolio,
+              experience: Data.experience,
+              sampleCode: Data.SampleCodeUrl,
+              preferedEnviroments: Data.preferedEnviroments,
+              availability: Data.availability,
+              projects: Data.projects,
+              quotes: Data.quotes
+          }));
+      }
+  }
+
+  handleSaveData(data) {
+    const { name, address, languages, imageURL, skills } = data;
+    this.setState((prevState, nextState) => ({
+        editMode: false,
+        profileData: {
+            name,
+            address,
+            languages,
+            imageURL,
+            skills
+        }
+    }));
+
+    setTimeout( () => {
+        localStorage.setItem(
+          "toptal-profile",
+          JSON.stringify(this.state)
+        );
+      }, 1 );
+  }
+
+  handleSavePortfolio(portfolio){
+    this.setState( () => ({ portfolio }))
+  }
+
+  handleEditMode() {
+    this.setState((prevState, nextState) => ({
+      editMode: !prevState.editMode
+    }));
+  }
+
+  render() {
+    const store = localStorage.getItem("toptal-profile") || {};
+    return (
+      <div>
+        <Header />
+        
+        <ProfileInfo
+          change={this.handleEditMode.bind(this)}
+          editMode={this.state.editMode}
+          saveData={this.handleSaveData.bind(this)}
+          data={this.state.profileData} />
+
+        <div className="row align-justify collapse">
+
+          <PortfolioSquare 
+            data={this.state.portfolio} 
+            handleSavePortfolio={this.handleSavePortfolio.bind(this)} 
+            editing={this.state.editMode} 
+            title="Portfolio" 
+            subtitle="PHP, Ruby, JavaScript" />
+
+          <ExperienceSquare 
+            data={this.state.experience} 
+            editing={this.state.editMode} 
+            title="Experience" />
+
+          <SampleCodeSquare 
+            title="Sample code and algorithms" />
+
+          <AvailabilitySquare />
+        </div>
+
+        <div className="row align-justify collapse">
+          <QuoteSquare title="The most amazing..." />
+          <QuoteSquare title="In clients I look for..." />
+          <MapLocationSquare address={this.state.profileData.address} />
+          <QuoteSquare title="Note" />
+        </div>
+
+        <div className="row align-justify collapse">
+          <h1>Let me introduce myself...</h1>
+        </div>
+
+        <div className="row align-justify collapse">
+          <QuoteSquare title="In clients I look for..." />
+        </div>
+
+        <div className="row align-justify">
+          <div className="columns">
+            <h1>To hire Martin join us as a</h1>
+            <button>Client</button>
+          </div>
+          <div className="columns">
+            <p>or as a developer</p>
+          </div>
+        </div>
+
+        <ToptalTopSkills />
+        <JoinMessageBox />
+        <Footer />
+      </div>
+    );
+  }
 }
 
-export default HomePage
+export default HomePage;
--- src/components/PortfolioSquare.js
+++ src/components/PortfolioSquare.js
@@ -1,22 +1,101 @@
 import React, { Component } from 'react'
+import EventEmitter from 'events';
+const emitter = new EventEmitter();
+
+console.log(emitter);
 
 class PortfolioSquare extends Component {
-    render () {
+    constructor(props){
+        super(props)
+        this.state = {
+            isEditing: this.props.editing,
+            isDirty: false,
+            portfolio: this.props.data
+        }
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+        this.props.handleProjects(this.state.portfolio)
+    }
+
+    handleChange(e){
+        e.target.value != "" ?
+            this.setState( ( ) => ({ isDirty: true }) ) :
+            this.setState( ( ) => ({ isDirty: false }) )
+    }
+
+    handleSavePortfolio(){
+        this.props.handleSavePortfolio(this.state.portfolio);
+    }
+
+    componentWillReceiveProps(nextProps){
+        if( this.setState.isEditing !== nextProps.editing ){
+            const elements = document.querySelectorAll('.portfolio__square .form__row');
+            const nextState = [];
+            elements.forEach( (row) => {
+                const project = row.children[0].value;
+                const techs = row.children[1].value;
+                if( project !== "" && techs !== "" )
+                    nextState.push({ title: project, skills: techs });
+            } )
+            new Promise( (resolve, reject) => {
+                this.setState( ( ) => ({ portfolio: nextState }) )
+                resolve();
+            })
+        }
+    }
+
+    renderFormRow(){
+        const content = []
+        for( let i = 0; i < 7; i++ ){
+            content.push( 
+                <div key={i} className="form__row">
+                    <input type="text" placeholder="Project name" defaultValue={this.props.data[i] ? this.props.data[i].title : '' } />
+                    <input type="text" placeholder="Skills used" defaultValue={this.props.data[i] ? this.props.data[i].skills : '' } />
+                </div>
+            )
+        }
+        return content;
+    }
+
+    renderForm(){
         return (
-            <div>
-                <h3>{this.props.title}</h3>
-                <span className="portfolio__subtitle">
-                    {this.props.subtitle}
-                </span>
-                <ul>
-                    <li><b>NavalPlan</b>, PHP, Ruby</li>
-                    <li><b>MyTime</b>, JavaScript</li>
-                    <li><b>Formidable</b>, PHP, Ruby</li>
-                    <li><b>SparkleShare</b>, JavaScript</li>
-                    <li><b>Monoon</b>, ActionScript</li>
-                </ul>
+            <div className="columns medium-3 small-12" >
+                <div className="profile__square profile__square--black portfolio__square">
+                    <h3 className="profile__square--title profile__square--title--white">{this.props.title}</h3>
+                    <form 
+                        onInput={this.handleSavePortfolio.bind(this)}
+                        onChange={this.handleChange.bind(this)} >
+                        { this.renderFormRow() }
+                    </form>
+                </div>
             </div>
         )
+    }
+
+    renderContent(){
+        return(
+            <div className="columns medium-3 small-12" >
+                <div className="profile__square">
+                    <h3 className="profile__square--title">{this.props.title}</h3>
+                    <span className="portfolio__subtitle">
+                        { this.props.subtitle }
+                    </span>
+                    <ul>
+                        {
+                            this.props.data.map( (project, index) => {
+                                return <li key={index}><b>{project.title}</b>, {project.skills}</li>
+                            })   
+                        }
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+
+    render () {
+        return this.props.editing ? this.renderForm() : this.renderContent(); 
     }
 }
 
