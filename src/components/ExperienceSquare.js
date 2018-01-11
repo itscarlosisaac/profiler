c1e46da5ef53108e394b5a94fb1e3ed18a8b3f57
Carlos Isaac Martinez Batista <itscarlosisaac@gmail.com>
2018-01-11 16:09:14 -0400

    Add: Content for experience square component.

--- src/components/ExperienceSquare.js
+++ src/components/ExperienceSquare.js
@@ -1,19 +1,92 @@
 import React, { Component } from 'react'
 
 class ExperienceSquare extends Component {
-    render () {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            isEditing: this.props.editing,
+            isDirty: false,
+            exp: this.props.data
+        }
+    }
+
+    handleChange(e){
+        e.target.value != "" ?
+            this.setState( ( ) => ({ isDirty: true }) ) :
+            this.setState( ( ) => ({ isDirty: false }) )
+    }
+
+    handleSaveExperience(){
+        this.props.handleSaveExperience(this.state.exp);
+    }
+
+    componentWillReceiveProps(nextProps){
+        if( this.setState.isEditing !== nextProps.editing ){
+            const elements = document.querySelectorAll('.experience__square .form__row');
+            const nextState = [];
+            elements.forEach( (row) => {
+                const exp = row.children[0].value;
+                const years = row.children[1].value;
+                if( exp !== "" && years !== "" )
+                    nextState.push({ experience: exp, years: years });
+            } )
+            new Promise( (resolve, reject) => {
+                this.setState( ( ) => ({ exp: nextState }) )
+                resolve();
+            })
+        }
+    }
+
+    renderFormRow(){
+        const content = []
+        for( let i = 0; i < 7; i++ ){
+            content.push( 
+                <div key={i} className="form__row">
+                    <input type="text" placeholder="Experience" defaultValue={this.props.data[i] ? this.props.data[i].experience : '' } />
+                    <input type="text" placeholder="Years" defaultValue={this.props.data[i] ? this.props.data[i].years : '' } />
+                </div>
+            )
+        }
+        return content;
+    }
+
+    renderForm(){
         return (
-            <div>
-                <h3>{this.props.title}</h3>
-                <ul>
-                    <li><b>PHP</b>, 6 years</li>
-                    <li><b>Ruby</b>, 7 years</li>
-                    <li><b>JavaScript</b>, 4 years</li>
-                    <li><b>ActionScript</b>, 3 years</li>
-                </ul>
+            <div className="columns medium-3 small-12" >
+                <div className="profile__square profile__square--black experience__square">
+                    <h3 className="profile__square--title profile__square--title--white">{this.props.title}</h3>
+                    <form 
+                        onInput={this.handleSaveExperience.bind(this)}
+                        onChange={this.handleChange.bind(this)} >
+                        { this.renderFormRow() }
+                    </form>
+                </div>
             </div>
         )
     }
+
+    renderContent(){
+        return(
+            <div className="columns medium-3 small-12" >
+                <div className="experience__square">
+                    <h3 className="profile__square--title">{this.props.title}</h3>
+                    <ul className="experience__list">
+                        {
+                            this.props.data.map( (exp, index) => {
+                                return <li key={index}><b>{exp.experience}</b>, {exp.years}</li>
+                            })   
+                        }
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+
+    render () {
+        return this.props.editing ? this.renderForm() : this.renderContent(); 
+    }
+
 }
 
 export default ExperienceSquare
