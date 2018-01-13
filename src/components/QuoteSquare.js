68520c3ff9b95f0cef0ad45b70a148cb1afc6df8
Carlos Isaac Martinez Batista <itscarlosisaac@gmail.com>
2018-01-13 03:09:08 -0400

    Refactor: Quote component.

--- src/components/QuoteSquare.js
+++ src/components/QuoteSquare.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import API from './utils/api'
 
 class QuoteSquare extends Component {
     getInitialState(){
@@ -10,9 +11,14 @@ class QuoteSquare extends Component {
         this.state = {
             editing: this.props.editing,
             quote: '',
-            author: this.props.name || '',
             mouseover:false
         }
+    }
+
+    componentWillMount(){
+        const store = API.getDataFromLocalStore(`toptal-quotes-${this.props.quoteNum}`) || ''
+        const name = API.getDataFromLocalStore('toptal-profile-info').name.split(' ')[0] || ''
+        this.setState( () => ({ quote:store, author: name }))
     }
 
     handleChange(e){
@@ -23,7 +29,7 @@ class QuoteSquare extends Component {
     }
 
     handleQuoteSave(){
-        this.props.handleQuoteSave(this.state.quote);
+        API.saveDataToLocalStore( (`toptal-quotes-${this.props.quoteNum}`), this.state.quote  )
     }
 
     handleMouseIn(){
@@ -43,13 +49,15 @@ class QuoteSquare extends Component {
             <div className="columns medium-3 small-12" >
                 <div 
                     className="profile__square profile__square--black quote__square" 
-                    onMouseEnter={this.handleMouseIn.bind(this)}
-                    onMouseLeave={this.handleMouseOut.bind(this)}>
+                    // onMouseEnter={this.handleMouseIn.bind(this)}
+                    // onMouseLeave={this.handleMouseOut.bind(this)}
+                >
                     <h3 className="profile__square--title profile__square--title--white">{this.props.title}</h3>
+
                     <form 
                         onInput={this.handleQuoteSave.bind(this)}
-                        onChange={this.handleChange.bind(this)} >
-                        <textarea className="quote__text__field" defaultValue={this.props.data}></textarea>
+                        onChange={this.handleChange.bind(this)}  >
+                        <textarea className="quote__text__field" defaultValue={this.state.quote}></textarea>
                     </form>
                 </div>
                 { 
@@ -66,8 +74,11 @@ class QuoteSquare extends Component {
                     <span className="profile__square--left--quote">&ldquo;</span>
                     <h3 className="profile__square--title">{this.props.title}</h3>
                     <blockquote className="profile__square--quote">
-                        <p> <em> {this.props.data} </em> </p>
-                        <span className="profile__square--quote--author"> {this.props.author != '' ? '' : '&ndash;'} {this.state.author} </span>
+                        <p> <em> {this.state.quote} </em> </p>
+                        <span className="profile__square--quote--author"> 
+                            {this.state.author == '' ? '' : '-' } 
+                            {this.state.author} 
+                        </span>
                     </blockquote>
                     <span className="profile__square--right--quote">&rdquo;</span>
                 </div>
